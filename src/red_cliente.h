fe95d073951a0cd7b8456daba64fa4436d83e2a6
Marcelo9987 <imarcelo8000@gmail.com>
2025-11-23 13:24:06 +0100

    He añadido a Víctor en el copyright

--- src/red_cliente.h
+++ src/red_cliente.h
@@ -1,5 +1,5 @@
- /*
-  Copyright (C) 2025. Marcelo Fort Muñoz
+/*
+  Copyright (C) 2025. Marcelo Fort Muñoz y Víctor Arrollo Marquez
 
      This program is free software: you can redistribute it and/or modify
      it under the terms of the GNU General Public License as published by
@@ -15,6 +15,7 @@
      along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
  */
+
 #pragma once
 
 #include <stdint.h>
